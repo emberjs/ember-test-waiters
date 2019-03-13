de414d04abf37f8544840a12eac61c74b459c98f
Steve Calvert <scalvert@linkedin.com>
2019-03-12 19:14:38 -0700

    Duh, error

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
   rules: {
     'prettier/prettier': 'error',
     'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': 'error',
   },
   overrides: [
     // node files
