868f82cec3ce39c6e4e9f7885ce032bcbe55b82b
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-04-04 13:05:38 -0400

    Add tests for native Promise

--- addon/wait-for-promise.ts
+++ addon/wait-for-promise.ts
@@ -1,5 +1,5 @@
 import { DEBUG } from '@glimmer/env';
-import { Promise as RSVPPromise } from 'rsvp';
+import RSVP from 'rsvp';
 import buildWaiter from './build-waiter';
 
 const PROMISE_WAITER = buildWaiter('promise-waiter');
@@ -8,7 +8,7 @@ const PROMISE_WAITER = buildWaiter('promise-waiter');
  * A convenient utility function to simplify waiting for a promise.
  *
  * @public
- * @param promise {Promise<T> | RSVPPromise<T>} The promise to track async operations for
+ * @param promise {Promise<T> | RSVP.Promise<T>} The promise to track async operations for
  * @param label {string} An optional string to identify the promise
  *
  * @example
@@ -25,7 +25,7 @@ const PROMISE_WAITER = buildWaiter('promise-waiter');
  *   }
  * }
  */
-export default function waitForPromise<T, PromiseType extends Promise<T> | RSVPPromise<T>>(
+export default function waitForPromise<T, PromiseType extends Promise<T> | RSVP.Promise<T>>(
   promise: PromiseType,
   label?: string
 ): PromiseType {
--- tests/unit/wait-for-promise-test.ts
+++ tests/unit/wait-for-promise-test.ts
@@ -3,7 +3,7 @@ import { _reset, getPendingWaiterState, waitForPromise } from 'ember-test-waiter
 import { module, test } from 'qunit';
 
 import { DEBUG } from '@glimmer/env';
-import { Promise } from 'rsvp';
+import RSVP from 'rsvp';
 
 if (DEBUG) {
   module('wait-for-promise', function(hooks) {
@@ -12,54 +12,82 @@ if (DEBUG) {
       resetError();
     });
 
-    test('waitForPromise wraps and registers a waiter', async function(assert) {
-      let promise = new Promise(resolve => {
-        resolve();
-      });
+    let promiseImplementations = [
+      { name: 'Native', CurrentPromise: Promise },
+      { name: 'RSVP', CurrentPromise: RSVP.Promise },
+    ];
 
-      overrideError(MockStableError);
+    for (let implementation of promiseImplementations) {
+      // Gets around the issue where TS says:
+      //
+      // Each member of the union type 'PromiseConstructor | typeof RSVP.Promise' has
+      // construct signatures, but none of those signatures are compatible with each other.
+      //
+      // In practice, RSVP.Promise and Native Promise are the exact same when it comes
+      // using waitForPromise.
+      //
+      // All of the below code is 100% type correct when 'CurrentPromise' is _either_
+      // Promise, or RSVP.Promise, but not Promise | RSVP.Promise
+      let { name, CurrentPromise } = (implementation as any) as {
+        name: string;
+        CurrentPromise: typeof Promise;
+      };
+      module(`Implementation: ${name}`, function() {
+        hooks.afterEach(function() {
+          _reset();
+          resetError();
+        });
 
-      promise = waitForPromise(promise);
+        test('waitForPromise wraps and registers a waiter', async function(assert) {
+          let promise = new CurrentPromise(resolve => {
+            resolve();
+          });
 
-      assert.deepEqual(getPendingWaiterState(), {
-        pending: 1,
-        waiters: {
-          'promise-waiter': [
-            {
-              label: undefined,
-              stack: 'STACK',
+          overrideError(MockStableError);
+
+          promise = waitForPromise(promise);
+
+          assert.deepEqual(getPendingWaiterState(), {
+            pending: 1,
+            waiters: {
+              'promise-waiter': [
+                {
+                  label: undefined,
+                  stack: 'STACK',
+                },
+              ],
             },
-          ],
-        },
+          });
+
+          await promise.then(() => {
+            assert.deepEqual(getPendingWaiterState(), { pending: 0, waiters: {} });
+          });
+        });
+
+        test('waitForPromise transitions waiter to not pending even if promise throws', async function(assert) {
+          let promise = CurrentPromise.resolve().then(() => {
+            throw new Error('Promise threw');
+          });
+
+          try {
+            await waitForPromise(promise).then();
+          } catch (e) {
+            assert.deepEqual(getPendingWaiterState(), { pending: 0, waiters: {} });
+          }
+        });
+
+        test('waitForPromise transitions waiter to not pending even if promise throws when thenable wrapped', async function(assert) {
+          let promise = CurrentPromise.resolve().then(() => {
+            throw new Error('Promise threw');
+          });
+
+          try {
+            await waitForPromise(promise.then());
+          } catch (e) {
+            assert.deepEqual(getPendingWaiterState(), { pending: 0, waiters: {} });
+          }
+        });
       });
-
-      await promise.then(() => {
-        assert.deepEqual(getPendingWaiterState(), { pending: 0, waiters: {} });
-      });
-    });
-
-    test('waitForPromise transitions waiter to not pending even if promise throws', async function(assert) {
-      let promise = Promise.resolve().then(() => {
-        throw new Error('Promise threw');
-      });
-
-      try {
-        await waitForPromise(promise).then();
-      } catch (e) {
-        assert.deepEqual(getPendingWaiterState(), { pending: 0, waiters: {} });
-      }
-    });
-
-    test('waitForPromise transitions waiter to not pending even if promise throws when thenable wrapped', async function(assert) {
-      let promise = Promise.resolve().then(() => {
-        throw new Error('Promise threw');
-      });
-
-      try {
-        await waitForPromise(promise.then());
-      } catch (e) {
-        assert.deepEqual(getPendingWaiterState(), { pending: 0, waiters: {} });
-      }
-    });
+    }
   });
 }
