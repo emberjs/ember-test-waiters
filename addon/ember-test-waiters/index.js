5ddc328ea5c809a43f620a290f1cb40cc3b16763
Steve Calvert <scalvert@linkedin.com>
2020-04-20 10:16:18 -0700

    Updates from PR feedback

--- addon/ember-test-waiters/index.js
+++ addon/ember-test-waiters/index.js
@@ -1,1 +1,12 @@
+import { deprecate } from '@ember/debug';
+
+deprecate(
+  'Importing from ember-test-waiters is deprecated. Please import from @ember/test-waiters',
+  true,
+  {
+    id: 'ember-test-waiters-legacy-module-name',
+    until: '3.0.0',
+  }
+);
+
 export * from '@ember/test-waiters';
