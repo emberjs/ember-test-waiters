3c365b276ab574deec52deaa9dcaf009f1229e44
Steve Calvert <steve.calvert@gmail.com>
2021-05-14 08:22:49 -0700

    fix: Flip the logic for deprecation of old package name

--- addon/ember-test-waiters/index.ts
+++ addon/ember-test-waiters/index.ts
@@ -2,7 +2,7 @@ import { deprecate } from '@ember/debug';
 
 deprecate(
   'Importing from ember-test-waiters is deprecated. Please import from @ember/test-waiters',
-  true,
+  false,
   {
     id: 'ember-test-waiters-legacy-module-name',
     until: '3.0.0',
