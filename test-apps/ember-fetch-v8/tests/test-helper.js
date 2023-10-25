9eac2c66e8a4c36306f65be30df48a5f4937818a
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-10-25 15:21:08 -0400

    Create 4 empty test apps

---
+++ test-apps/ember-fetch-v8/tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'ember-fetch-v8/app';
+import config from 'ember-fetch-v8/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
