120b5992240c516481d2e6ce944ad7bbcb3b85e1
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-05-06 13:41:40 -0400

    Test against embroider (#479)
    * Also test against strictest embroider
    
    * Add missing dependencies
    
    * Add maybeEmbroider to the other test-apps
    
    * more default
    
    * nevermind about the ember-concurrency test-app, it seems incompatible

--- test-apps/base-tests/ember-cli-build.js
+++ test-apps/base-tests/ember-cli-build.js
@@ -9,5 +9,25 @@ module.exports = function (defaults) {
     // Add options here
   });
 
+  if (process.env.EMBROIDER) {
+    const { Webpack } = require('@embroider/webpack');
+
+    return require('@embroider/compat').compatBuild(app, Webpack, {
+      extraPublicTrees: [],
+      staticAddonTrees: true,
+      staticAddonTestSupportTrees: true,
+      staticHelpers: true,
+      staticModifiers: true,
+      staticComponents: true,
+      staticEmberSource: true,
+      packagerOptions: {
+        webpackConfig: {
+          // Highest fidelity source map (at the cost of build speed)
+          devtool: 'source-map',
+        },
+      },
+    });
+  }
+
   return app.toTree();
 };
