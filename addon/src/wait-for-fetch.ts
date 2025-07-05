c6bcd25d3e8f3856ed894e56febe43ee000b134c
Brad Overton <bo.inbox+github@protonmail.ch>
2025-07-05 12:21:11 +1000

    refactor: improve types in wait-for-fetch

--- addon/src/wait-for-fetch.ts
+++ addon/src/wait-for-fetch.ts
@@ -1,18 +1,37 @@
 import { default as waitForPromise } from './wait-for-promise.ts';
 
+const properties = [
+  'ok',
+  'status',
+  'statusText',
+  'bodyUsed',
+  'headers',
+  'redirected',
+  'type',
+  'url',
+] as const satisfies (keyof Response)[];
+type ResponseProp = (typeof properties)[number];
+function isResponseProperty(maybeProp: string): maybeProp is ResponseProp {
+  return properties.some((prop) => maybeProp === prop);
+}
+
+/**
+ * Wraps the fetch promise in a test waiter, and also wraps the returned promises' async functions (like json()) in a
+ * test waiter.
+ */
 export async function waitForFetch(fetchPromise: ReturnType<typeof fetch>) {
   const response = await waitForPromise(fetchPromise);
 
   return new Proxy(response, {
     get(target, prop, receiver) {
-      if (
-        typeof prop === 'string' &&
-        ['ok', 'status', 'statusText', 'bodyUsed', 'headers', 'redirected', 'type', 'url'].includes(prop)
-      ) {
+      /* Depending on the stack, Response is often already a Proxy. Reflect.get() will error for property values, when
+       * using a Proxy as the _receiver_ arg, so just return the value the normal way to avoid that issue. */
+      if (typeof prop === 'string' && isResponseProperty(prop)) {
         return target[prop];
       }
       const original = Reflect.get(target, prop, receiver);
 
+      // For async functions, call them but wrapped in waitForPromise
       if (
         typeof prop === 'string' &&
         ['json', 'text', 'arrayBuffer', 'blob', 'formData', 'bytes'].includes(prop)
@@ -21,10 +40,11 @@ export async function waitForFetch(fetchPromise: ReturnType<typeof fetch>) {
           return waitForPromise(original.call(target, ...args));
         };
       }
+      // For sync functions, just call them
       if (typeof prop === 'string' && ['clone'].includes(prop)) {
-        return (...args) => {
+        return (...args: unknown[]) => {
           return original.call(target, ...args);
-        }
+        };
       }
       return original;
     },
